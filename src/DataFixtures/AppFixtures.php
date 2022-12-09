2040ca07a1c56551c16ad10a5b94642df3d4d533
MamadiKB <kbm.dev91@gmail.com>
2022-12-09 15:20:29 +0100

    feat: Fake Establishment fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,33 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Establishment;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // Création d'une vingtaine de restaurants ayant pour titre
+        $typeArray = ['Restaurant', 'Izakaya'];
+        for ($i = 0; $i < 20; $i++) {
+            $key = array_rand($typeArray);
+            $establishment = new Establishment;
+            $establishment->setName('establishment ' . $i);
+            $establishment->setType($typeArray[$key]);
+            $establishment->setDescription('lorem ' . $i);
+            $establishment->setAddress('rue ' . $i);
+            $establishment->setPrice(30);
+            $establishment->setWebsite('https: ' . $i);
+            $establishment->setPhone(689613315);
+            $establishment->setRating(2.3);
+            $establishment->setSlug('slug ' . $i);
+            $establishment->setPicture('https: ' . $i);
+            $establishment->setStatus(1);
+            $establishment->setOpeningTime('du ' . $i . ' a ' .$i);
+            $manager->persist($establishment);
+        }
 
         $manager->flush();
     }
