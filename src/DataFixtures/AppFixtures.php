a4b947348e08cec3e55f189609483bc41fe37c32
MamadiKB <kbm.dev91@gmail.com>
2022-12-09 17:11:51 +0100

    feat: Fake District fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\District;
 use App\Entity\Establishment;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -10,7 +11,20 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // Création d'une vingtaine de restaurants ayant pour titre
+        // Création des district.
+        $listDistrict = [];
+        for ($i = 0; $i < 12; $i++) {
+            // Création du district lui-même.
+            $district = new District();
+            $district->setName("the" . $i);
+            $district->setKanji("漢字" . $i);
+            $district->setSlug("/the" . $i);
+            $manager->persist($district);
+            // On sauvegarde le district créé dans un tableau.
+            $listDistrict[] = $district;
+        }
+
+        // Création d'une vingtaine d'establishment ayant pour titre
         $typeArray = ['Restaurant', 'Izakaya'];
         for ($i = 0; $i < 20; $i++) {
             $key = array_rand($typeArray);
@@ -27,6 +41,8 @@ class AppFixtures extends Fixture
             $establishment->setPicture('https: ' . $i);
             $establishment->setStatus(1);
             $establishment->setOpeningTime('du ' . $i . ' a ' .$i);
+            // On lie le district à un establishment pris au hasard dans le tableau des auteurs.
+            $establishment->setDistrict($listDistrict[array_rand($listDistrict)]);
             $manager->persist($establishment);
         }
 
