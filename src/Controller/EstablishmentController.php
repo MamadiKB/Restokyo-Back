6fafe69da8cead2477ac657e2d6ab428283fb907
MamadiKB <kbm.dev91@gmail.com>
2022-12-11 19:24:19 +0100

    feat: add updateEstablishment

--- src/Controller/EstablishmentController.php
+++ src/Controller/EstablishmentController.php
@@ -3,16 +3,17 @@
 namespace App\Controller;
 
 use App\Entity\Establishment;
+use App\Repository\TagRepository;
 use App\Repository\DistrictRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\EstablishmentRepository;
-use App\Repository\TagRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class EstablishmentController extends AbstractController
@@ -41,7 +42,9 @@ class EstablishmentController extends AbstractController
     }
 
     #[Route('/api/establishment', name:"createEstablishment", methods: ['POST'])]
-    public function createEstablishment(Request $request, SerializerInterface $serializer, EntityManagerInterface $em, UrlGeneratorInterface $urlGenerator, DistrictRepository $districtRepository, TagRepository $tagRepository): JsonResponse 
+    public function createEstablishment(Request $request, SerializerInterface $serializer, 
+                                        EntityManagerInterface $em, UrlGeneratorInterface $urlGenerator, 
+                                        DistrictRepository $districtRepository, TagRepository $tagRepository): JsonResponse 
     {
         $establishment = $serializer->deserialize($request->getContent(), Establishment::class, 'json');
         // Récupération de l'ensemble des données envoyées sous forme de tableau
@@ -52,7 +55,7 @@ class EstablishmentController extends AbstractController
         // On cherche le district qui correspond et on l'assigne a l'establishment.
         // Si "find" ne trouve pas le district, alors null sera retourné.
         $establishment->setDistrict($districtRepository->find($idDistrict));
-        
+
         // Récupération de l'idTags. S'il n'est pas défini, alors on met -1 par défaut.
         $idTags = $content['idTags'] ?? -1;
         // On parcoure le tableu idTags qui contien l'id des tag.
@@ -67,7 +70,29 @@ class EstablishmentController extends AbstractController
         $jsonEstablishment = $serializer->serialize($establishment, 'json', ['groups' => 'getEstablishment']);
         $location = $urlGenerator->generate('getOnEstablishment', ['id' => $establishment->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
         return new JsonResponse($jsonEstablishment, Response::HTTP_CREATED, ["Location" => $location], true);
-
     }
+
+    #[Route('/api/establishment/{id}', name:"updateEstablishment", methods:['PUT'])]
+    public function updateEstablishment(Request $request, SerializerInterface $serializer,
+                                        Establishment $currentEstablishment, EntityManagerInterface $em, 
+                                        DistrictRepository $districtRepository, TagRepository $tagRepository): JsonResponse 
+    {
+        $updatedEstablishment = $serializer->deserialize($request->getContent(), 
+                Establishment::class, 
+                'json', 
+                [AbstractNormalizer::OBJECT_TO_POPULATE => $currentEstablishment]);
+        // Récupération de l'ensemble des données envoyées sous forme de tableau
+        $content = $request->toArray();
+        // Récupération de l'idDistrict. S'il n'est pas défini, alors on retourne le district de base.
+        $idDistrict = $content['idDistrict'] ?? $currentEstablishment->getDistrict();
+        // On cherche le district qui correspond et on l'assigne a l'establishment.
+        // Si "find" ne trouve pas le district, alors null sera retourné.
+        $updatedEstablishment->setDistrict($districtRepository->find($idDistrict));
+        
+        $em->persist($updatedEstablishment);
+        $em->flush();
+
+        return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT);
+   }
     
 }
